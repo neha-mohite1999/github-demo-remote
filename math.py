f1c462f30cf1efcf6324ce24ecba46c6c0aeff00
neha-mohite1999 <90001287+neha-mohite1999@users.noreply.github.com>
2021-09-03 15:52:13 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,13 @@
+# add implementation
+def add(x,y):
+    return x+y
+#sub
+def substract(x,y):
+    return x-y #on master
+ #multiply   
+def multiply(x,y):
+    return x*y #onbug
+ #divide   
+def divide(x,y):
+    pass
+    
